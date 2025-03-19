d1e1c6fa136660b599893b44a82e7922551513e8
Benjamín Maureira Flores <91946757+benjimrfl@users.noreply.github.com>
2025-03-19 16:33:11 +0100

    Delete backend directory

--- backend/fisio_find/fisio_find/settings_postman.py
+++
@@ -1,9 +0,0 @@
-from .settings import *
-
-# Configuración de la base de datos para testing
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'test_db.sqlite3',  # Puedes usar ':memory:' para una DB en memoria
-    }
-}
